0d4055d939e651df97f930d79e63066596439d6b
Nallive Trujillo <nallive14@gmai.com>
2023-07-12 17:02:45 -0500

    Home en construcción

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "./Home";
 
-function App() {
-  
+const App = () => {
+  const handleButtonClick = (name: string) => {
+    console.log("Button clicked with name:", name);
+  };
   return (
-    <>
-     
-    </>
-  )
-}
+    <div>
+      <Home onButtonClick={handleButtonClick} />
+    </div>
+  );
+};
 
-export default App
+export default App;
