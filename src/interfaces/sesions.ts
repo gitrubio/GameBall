f6391722ba044deacd3219b7a68051c807fd6ad2
lortiz24 <luis.ortiz@evius.co>
2023-07-12 15:37:16 -0500

    Se crean interfaces para las sesiones y players

---
+++ src/interfaces/sesions.ts
@@ -0,0 +1,18 @@
+
+
+export type StatusSession = 'pending' | 'playing' | 'finished'
+
+export interface ISession {
+    dateStart: Date;
+    dateEnd: Date;
+    players: IPlayer[]
+    statusSession: StatusSession
+}
+
+export type StatusPlayed = 'pending' | 'playing' | 'finished'
+export interface IPlayer {
+    name: string;
+    startPlay: Date;
+    endPlay: Date;
+    status: StatusPlayed
+}
