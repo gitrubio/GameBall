43338455651972a4d01382f5b5bde8c595064d4c
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-07-12 18:12:08 -0500

    add : tratando de escuchar el evento

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,44 +1,67 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Canvas } from 'react-three-fiber';
-import { OrbitControls, Sphere } from '@react-three/drei';
+import React, { useEffect, useRef } from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, Sphere } from "@react-three/drei";
+import { Button } from "antd";
 
 const Scene: React.FC = () => {
-  const [spherePosition, setSpherePosition] = useState<[number, number, number]>([0, 0, 0]);
+  const sphereRef = useRef<any>();
   const speed = 0.1; // Factor de velocidad
 
   useEffect(() => {
     const handleDeviceOrientation = (event: DeviceOrientationEvent) => {
-      const { beta, gamma } = event;
-      const x = (gamma || 0) / 90; // Normalizar el valor gamma entre -1 y 1
-      const y = (beta || 0) / 90; // Normalizar el valor beta entre -1 y 1
+      if (sphereRef.current) {
+        const { beta, gamma } = event;
+        const x = (gamma || 0) / 90; // Normalizar el valor gamma entre -1 y 1
+        const y = (beta || 0) / 90; // Normalizar el valor beta entre -1 y 1
 
-      setSpherePosition(prevPosition => [
-        prevPosition[0] + x * speed, // Mover la esfera en el eje X
-        prevPosition[1] + y * speed, // Mover la esfera en el eje Y
-        prevPosition[2] // Mantener la posición en el eje Z
-      ]);
+        sphereRef.current.position.x = x * speed; // Ajustar la velocidad de movimiento
+        sphereRef.current.position.y = y * speed; // Ajustar la velocidad de movimiento
+      }
     };
-
-    if (typeof DeviceOrientationEvent !== 'undefined') {
+ 
       window.addEventListener('deviceorientation', handleDeviceOrientation);
-    } else {
-      console.error('El dispositivo no admite el acceso a la orientación.');
-    }
 
     return () => {
       window.removeEventListener('deviceorientation', handleDeviceOrientation);
     };
   }, []);
 
+  function testDeviceOrientation() {
+
+    if (typeof DeviceOrientationEvent !== "function") {
+     alert("DeviceOrientationEvent not detected");
+    }
+    if (typeof DeviceOrientationEvent.requestPermission !== "function") {
+      alert("DeviceOrientationEvent.requestPermission not detected");
+    }
+    DeviceOrientationEvent.requestPermission().then(function (result : any) {
+    
+      window.addEventListener("deviceorientation", handleDeviceOrientation, true);
+      alert(result);
+    });
+  }
   return (
-    <Canvas style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <OrbitControls />
-      <Sphere args={[0.7, 32, 32]} position={spherePosition}>
-        <meshPhongMaterial color="red" />
-      </Sphere>
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          background: "violet",
+        }}
+      >
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <OrbitControls />
+        <Sphere ref={sphereRef} args={[0.5, 32, 32]} position={[0, 0, 0]}>
+          <meshPhongMaterial color="red" />
+        </Sphere>
+        <OrbitControls />
+      </Canvas>
+        <Button onClick={testDeviceOrientation}>Permisos</Button>
+    </>
   );
 };
 
