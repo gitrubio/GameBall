290328deaa171b8dcc756cc3bc3eaed38041ad22
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-07-12 17:09:25 -0500

    add : escenario

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Scene from './components/Scene'
 
 function App() {
   
   return (
     <>
-     
+     <div style={{ width: '10 0%', height: '100vh', overflow: 'hidden' }}>
+      <Scene />
+    </div>
     </>
   )
 }
